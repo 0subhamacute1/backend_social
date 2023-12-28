2ffb8d35dcf7ba5520b409ef88a345f9688b0598
0subhamacute1 <subham.gupta@acuteinformatics.in>
2023-12-28 19:01:52 +0530

    a

--- route/user.js
+++ route/user.js
@@ -14,7 +14,8 @@ router.post("/create", async (req, res) => {
       resHandler(res, 400, false, "user already exist");
     } else {
       if (!req.body?.name) return resHandler(res, 400, false, "name empty");
-      if (!Number(req.body?.phone)) return resHandler(res, 400, false, "phone empty");
+      if (!Number(req.body?.phone))
+        return resHandler(res, 400, false, "phone empty");
       if (!req.body?.password)
         return resHandler(res, 400, false, "password empty");
 
@@ -27,15 +28,6 @@ router.post("/create", async (req, res) => {
       resHandler(res, 200, true, "user created", result);
     }
   } catch (err) {
-    console.log("err 500 signup", err);
-    resHandler(res, 500, false, err);
-  }
-});
-
-router.post("/login", async (req, res) => {
-  try {
-  } catch (err) {
-    console.log("err 500 login", err);
     resHandler(res, 500, false, err);
   }
 });
@@ -50,7 +42,24 @@ router.get("/userlist", async (req, res) => {
         resHandler(res, 500, false, err);
       });
   } catch (err) {
-    console.log("err 500 userlist", err);
+    resHandler(res, 500, false, err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+  } catch (err) {
+    resHandler(res, 500, false, err);
+  }
+});
+
+router.get("/userbyid/:id", async (req, res) => {
+  console.log("req.params", req.params);
+  try {
+    const user = await USER.findOne({ _id: req.params.id });
+    if (!user) return resHandler(res, 400, false, "user not found");
+    resHandler(res, 200, true, "user by id", user);
+  } catch (err) {
     resHandler(res, 500, false, err);
   }
 });
