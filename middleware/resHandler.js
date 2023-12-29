c9e573669d9772185bd2922f2c6d760ad87cef24
0subhamacute1 <subham.gupta@acuteinformatics.in>
2023-12-29 10:36:33 +0530

    a

--- middleware/resHandler.js
+++ middleware/resHandler.js
@@ -1,17 +1,9 @@
 const resHandler = (res, code, isSuccess, msg, data) => {
-  try {
-    return res.status(code).send({
-      success: isSuccess,
-      msg: msg ? msg : "no msg",
-      data: data ? data : "",
-    });
-  } catch (error) {
-    res.status(500).send({
-      success: false,
-      msg: "internal server err",
-      data: error,
-    });
-  }
+  return res.status(code).send({
+    success: isSuccess,
+    msg: msg ? msg : "no msg",
+    data: data ? data : "",
+  });
 };
 
 module.exports = resHandler;
